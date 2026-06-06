b8a7d7b3e93fdf204b08142ceb70ba7bb4f5e777
NgocHoangDc12 <namchemgio5@gmail.com>
2026-06-07 01:10:03 +0700

    Implement PhotoRouter for photo management

---
+++ PhotoRouter.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Photo = require("../db/photoModel");
+const User = require("../db/userModel");
+const router = express.Router();
+
+router.get("/:id", async (request, response) => {
+  const id = request.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return response.status(400).json({ message: "Invalid user id: " + id });
+  }
+
+  try {
+
+    const user = await User.findById(id);
+    if (!user) {
+      return response.status(400).json({ message: "User not found with id: " + id });
+    }
+
+    const photos = await Photo.find({ user_id: id }).select(
+      "_id user_id comments file_name date_time"
+    );
+
+    const processedPhotos = await Promise.all(
+      photos.map(async (photo) => {
+        const photoObj = {
+          _id: photo._id,
+          user_id: photo.user_id,
+          file_name: photo.file_name,
+          date_time: photo.date_time,
+          comments: [],
+        };
+
+        if (photo.comments && photo.comments.length > 0) {
+          photoObj.comments = await Promise.all(
+            photo.comments.map(async (comment) => {
+              const commentUser = await User.findById(comment.user_id).select(
+                "_id first_name last_name"
+              );
+
+              return {
+                _id: comment._id,
+                comment: comment.comment,
+                date_time: comment.date_time,
+                user: commentUser
+                  ? {
+                      _id: commentUser._id,
+                      first_name: commentUser.first_name,
+                      last_name: commentUser.last_name,
+                    }
+                  : null,
+              };
+            })
+          );
+        }
+
+        return photoObj;
+      })
+    );
+
+    response.status(200).json(processedPhotos);
+  } catch (error) {
+    console.error("Error fetching photos:", error);
+    response.status(400).json({ message: "Invalid user id: " + id });
+  }
+});
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  }
+});
+const upload = multer({ storage: storage });
+
+router.post("/new", upload.single("photo"), async (request, response) => {
+  if (!request.file) {
+    return response.status(400).send("No file uploaded");
+  }
+
+  try {
+    const newPhoto = await Photo.create({
+      file_name: request.file.filename,
+      date_time: new Date().toISOString(),
+      user_id: request.session.user._id,
+      comments: []
+    });
+
+    response.status(200).send(newPhoto);
+  } catch (error) {
+    console.error("Error creating photo:", error);
+    response.status(500).send("Internal server error");
+  }
+});
+
+module.exports = router;
